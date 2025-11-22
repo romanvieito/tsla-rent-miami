316c8fa3d53c5b9bba56b2960009f429fb304cbd
Alberto Roman <romanvieito@gmail.com>
2025-11-22 12:38:18 -0500

    Add Tawk.to live chat widget to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,19 @@ export default function RootLayout({
         <MixpanelProvider>
           {children}
         </MixpanelProvider>
+        <Script id="tawk-to-chat" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6921f49adadafd196019646e/1jama6utb';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
